787ade6e1c47f47f2e5f042a26bed2efbc3750f8
rodrigoberino <r9.berino@gmail.com>
2025-05-18 22:37:35 -0300

    Add - utils to the main function

--- bin/isurus_p.dart
+++ bin/isurus_p.dart
@@ -1,7 +1,31 @@
 import 'dart:io';
 
 void main() {
-  var trend =   stdin.readLineSync();
-  var obj = stdin.readLineSync();
-  print(trend! + obj! );
+  int trend =  int.parse(stdin.readLineSync()!);
+  int obj = int.parse(stdin.readLineSync()!);
+
+  void sum()
+  {
+    print(trend + obj);    
+  }
+  
+  void sub()
+  {
+    print(trend - obj);    
+  }
+
+  void mult()
+  {
+    print(trend * obj);    
+  }
+
+  void div()
+  {
+    print(trend / obj);    
+  }
+
+  sum();
+  sub();
+  mult();
+  div();
 }
